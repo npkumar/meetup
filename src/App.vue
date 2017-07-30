f613a8fe37be8eb81eeecbed9a40793c80ff2526
Nitin <getnpk@gmail.com>
2017-07-30 17:56:00 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>

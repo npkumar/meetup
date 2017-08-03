6338f57385515ccd38b04d21e8039d905316cb0c
Nitin <getnpk@gmail.com>
2017-08-03 23:28:54 +0800

    Add a carousel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,19 @@
 <template>
   <v-container>
     <v-layout row wrap>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+          v-for="meetup in meetups"
+          :src="meetup.imageUrl"
+          :key="meetup.id">
+          <div class="title">{{ meetup.title }}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap>
       <v-flex xs12 sm6 class="text-xs-center text-sm-right">
         <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
       </v-flex>
@@ -10,3 +23,36 @@
     </v-layout>
   </v-container>
 </template>
+
+<<script>
+export default {
+  data () {
+    return {
+      meetups: [
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/18/Hong_Kong_Night_Skyline.jpg',
+          id: 'id1',
+          title: 'Hong Kong'
+        },
+        {
+          imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/70/Ginza_at_Night%2C_Tokyo.jpg',
+          id: 'id2',
+          title: 'Tokyo'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Style only for this component -->
+<style scoped>
+  .title {
+    position: absolute;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0.5);
+    font-size: 2em;
+    color: white;
+    padding: 20px;
+  }
+</style>

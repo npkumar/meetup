8fbc3a740fb770e74519d6046ecd8cc1e28b4fdf
Nitin <getnpk@gmail.com>
2017-08-05 12:32:55 +0800

    Add a single meetup page

---
+++ src/components/Meetup/Meetup.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+      <v-layout row wrap>
+          <v-flex xs12>
+              <v-card>
+                <v-card-title>
+                    <h6 class="primary--text">HK Meetup</h6>
+                </v-card-title>
+
+                <v-card-media
+                  src='https://upload.wikimedia.org/wikipedia/commons/1/18/Hong_Kong_Night_Skyline.jpg'
+                  height="300px"
+                ></v-card-media>
+
+                <v-card-text>
+                    <div class="info--text">05 August 2017 - HK</div>
+                    <div>loream ipsum</div>
+                </v-card-text>
+
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn class="primary">Register</v-btn>
+                </v-card-actions>
+              </v-card>
+          </v-flex>
+      </v-layout>
+  </v-container>
+</template>

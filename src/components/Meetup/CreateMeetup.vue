70e23b3ff597086b5fdb454ce5d0a940dbfabb6e
Nitin <getnpk@gmail.com>
2017-08-06 10:36:30 +0800

    Add orginze meetup form

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,5 +1,88 @@
 <template>
-  <div>
-      Create Meetup
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4>Create new meetup</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="title"
+              label="Title"
+              id="title"
+              v-model="title"
+              required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="location"
+              label="Location"
+              id="location"
+              v-model="location"
+              required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="imageUrl"
+              label="Image URL"
+              id="image-url"
+              v-model="imageUrl"
+              required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl">
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="description"
+              label="Description"
+              id="description"
+              v-model="description"
+              multi-line
+              required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary">Register</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        title: '',
+        location: '',
+        imageUrl: '',
+        description: ''
+      }
+    }
+  }
+</script>

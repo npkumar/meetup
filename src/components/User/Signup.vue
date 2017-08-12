f2734de604234df7e049cdc78621981c26a1a8ab
Nitin <getnpk@gmail.com>
2017-08-12 09:12:41 +0800

    Add signup form

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,81 @@
 <template>
-  <div>
-      Sign Up
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Email"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required>
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required>
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      v-model="confirmPassword"
+                      type="password"
+                      :rules=[comparePasswords]>
+                    </v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-btn type="submit">SignUp</v-btn>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        confirmPassword: ''
+      }
+    },
+    computed: {
+      comparePasswords () {
+        return this.password !== this.confirmPassword ? 'Passwords do not match' : true
+      }
+    },
+    methods: {
+      onSignUp () {
+        console.log(this.email, this.password, this.confirmPassword)
+      }
+    }
+  }
+</script>
